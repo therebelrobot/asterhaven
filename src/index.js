c3eaacfe7be35e0387ab234533f2680040eaf7ee
Oz Haven (Trent Oswald) <ozzyhaven@therebelrobot.com>
2018-12-21 14:47:01 -0800

    updated with appropriate links

--- src/index.js
+++ src/index.js
@@ -2,17 +2,48 @@
 
 const boxen = require('boxen')
 const { bold, yellow, cyan } = require('chalk').default
+const moment = require('moment')
+
+function getGreetingTime () {
+  let g = null // return g
+  const m = moment()
+  const splitAfternoon = 12 // 24hr time to split the afternoon
+  const splitEvening = 17 // 24hr time to split the evening
+  const currentHour = parseFloat(m.format('HH'))
+
+  if (currentHour >= splitAfternoon && currentHour <= splitEvening) {
+    g = 'afternoon'
+  } else if (currentHour >= splitEvening) {
+    g = 'evening'
+  } else {
+    g = 'morning'
+  }
+  return `${moment().format('dddd')} ${g}`
+}
 
 const info = [
-  bold('Siarhei Melnik'),
-  'Senior Software Engineer at EPAM Systems',
+  bold('Oz Haven'),
+  'Senior Software Engineer at Saleae Inc.',
   '',
-  // `${bold('Work:     ')}${cyan('Senior Software Engineer at EPAM Systems')}`,
-  `${bold('GitHub:   ')}${cyan('https://github.com/SiarheiMelnik')}`,
-  `${bold('Twitter:  ')}${cyan('https://twitter.com/m_siarhei')}`,
-  `${bold('LinkedIn: ')}${cyan(
-    'https://www.linkedin.com/in/siarhei-melnik-a8b77976/'
-  )}`
+  `${bold('GitHub:   ')}${cyan('https://github.com/therebelrobot')}`,
+  `${bold('Twitter:  ')}${cyan('https://twitter.com/therebelrobot')}`,
+  `${bold('LinkedIn: ')}${cyan('https://www.linkedin.com/in/therebelrobot/')}`,
+  '------------------------',
+  '',
+  bold('The Forge Project'),
+  `Easy-listening synth-trap for your ${getGreetingTime()}.`,
+  cyan('https://theforge.pro'),
+  '',
+  `${bold('Spotify:   ')}${cyan(
+    'https://open.spotify.com/artist/4a56tUWRygifILXCoXAcoP?si=TeRAgIgPSdmEmf8Q04QjpQ'
+  )}`,
+  `${bold('Apple Music:  ')}${cyan(
+    'https://itunes.apple.com/us/artist/the-forge-project/1305199849'
+  )}`,
+  `${bold('Bandcamp: ')}${cyan(
+    'https://theforgeproject.bandcamp.com/releases'
+  )}`,
+  ''
 ]
 
 const style = {
